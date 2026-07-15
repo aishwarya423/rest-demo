8385f932cb38712a3e8eb2c1a8e94a30f2008259
Aishwarya <aishwarya.palarapu@gmail.com>
2026-07-15 14:34:46 +0530

    feat: Added docker service for graphiQL

--- graphiql-vite/src/App.jsx
+++ graphiql-vite/src/App.jsx
@@ -1,29 +1,53 @@
 import { GraphiQL } from 'graphiql';
+import { createGraphiQLFetcher } from '@graphiql/toolkit';
 import { explorerPlugin } from '@graphiql/plugin-explorer';
 
 import 'graphiql/style.css';
 import '@graphiql/plugin-explorer/style.css';
+ 
+
+const fetcher = createGraphiQLFetcher({
+  url : "http://127.0.0.1:5050/graphql",
+});
+
+const defaultQuery = `query InsurancePortfolio {
+  account(id: "acct-1001") {
+    id
+    holderName
+    accountType
+    totalValue
+    policies {
+      policyNumber
+      productName
+      status
+      linkedFunds {
+        name
+        assetClass
+        oneYearReturnPercent
+      }
+    }
+    fundHoldings {
+      allocationPercent
+      currentValue
+      fund {
+        name
+        riskRating
+        sustainabilityLabel
+      }
+    }
+  }
+}`;
 
 const explorer = explorerPlugin();
-async function fetcher(graphQLParams) {
-  const response = await fetch('http://127.0.0.1:5050/graphql', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(graphQLParams),
-  });
-
-  return response.json();
-}
 
 export default function App() {
   return (
     <GraphiQL
       fetcher={fetcher}
+      defaultQuery={defaultQuery}
       plugins={[explorer]}
       storage={window.localStorage}
+      // defaultEditorToolsVisibility={true}
     />
   );
 }
